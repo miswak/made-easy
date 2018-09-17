a13c0761f682c0d8d2fb99ac6eae53166b0632ac
Mohamed Farah <33624115+miswak@users.noreply.github.com>
2018-09-17 09:51:06 +0100

    Update Authentication.js

--- Authentication.js
+++ Authentication.js
@@ -12,7 +12,7 @@ export default {
   },
   async fetchCategoryDetail(catId) {
     try { //GET
-      const response = await fetch(apiHost +'/content/categories?token=eceede17f50442d284ed1a6b4a20b2f53918a928&parentid='+ catId);
+      const response = await fetch(apiHost +'/content?token=eceede17f50442d284ed1a6b4a20b2f53918a928&page=0&catid='+ catId);
       const responseJson = await response.json();
       return responseJson;
     } catch(error) {
