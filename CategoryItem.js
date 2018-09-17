6874ed27e677aa21ef18df6f827968fa06aaae71
Mohamed Farah <33624115+miswak@users.noreply.github.com>
2018-09-17 09:52:09 +0100

    Update CategoryItem.js

--- CategoryItem.js
+++ CategoryItem.js
@@ -6,7 +6,7 @@ import {
     TouchableOpacity
 } from "react-native";
 import Proptypes from 'prop-types'
-import priceDisplay from '../networking/util'
+import images from '../networking/util'
 
 
 class CategoryItem extends Component {
@@ -19,16 +19,69 @@ class CategoryItem extends Component {
     };
     render() {
         const {box} = this.props;
-        return (
+
+        var strImageURL = "";
+        switch(box.catId) {
+            case '59085af44bc6b0d3074933b6':
+                strImageURL = images.Accommodation;
+                break;
+            case '59085aff4bc6b0d3074933b7':
+                strImageURL = images.Activities;
+                break;
+            case '59085b074bc6b0d3074933b8':
+                strImageURL = images.Attractions;
+                break;
+            case '59085b114bc6b0d3074933b9':
+                strImageURL = images.Nightlife;
+                break;
+            case '59085b1b4bc6b0d3074933ba':
+                strImageURL = images.Restaurants;
+                break;
+            case '59085b244bc6b0d3074933bb':
+                strImageURL = images.Shops;
+                break;
+            case '59085b2f4bc6b0d3074933bc':
+                strImageURL = images.Transport;
+                break;
+
+        }
+        return   <View style ={styles.deal}>
+                <TouchableOpacity style={styles.info} onPress = {this.handlePress}>
+                    <Image style ={styles.image} source={strImageURL}/> 
+                        <Text style ={styles.title}>{box.name}</Text>          
+                </TouchableOpacity>  
+                    </View>
+/*
+                
+        
+        if(box.catId=='59085b114bc6b0d3074933b9'){
+            return   <TouchableOpacity style={styles.info} onPress = {this.handlePress}>
+                        <Image style ={styles.image} source={images.Nightlife}/> 
+                            <Text style ={styles.title}>{box.name}</Text>          
+                    </TouchableOpacity>  
+                    }
+        if(box.catId=='59085b1b4bc6b0d3074933ba'){
+            return   <TouchableOpacity style={styles.info} onPress = {this.handlePress}>
+                        <Image style ={styles.image} source={images.Restaurants}/> 
+                            <Text style ={styles.title}>{box.name}</Text>          
+                    </TouchableOpacity>  
+                    }
+        if(box.catId=='59085b244bc6b0d3074933bb'){
+            return   <TouchableOpacity style={styles.info} onPress = {this.handlePress}>
+                        <Image style ={styles.image} source={images.Shops}/> 
+                            <Text style ={styles.title}>{box.name}</Text>          
+                    </TouchableOpacity>  
+                    }
+        if(box.catId=='59085b2f4bc6b0d3074933bc'){
+            return   <TouchableOpacity style={styles.info} onPress = {this.handlePress}>
+                        <Image style ={styles.image} source={images.Transport}/> 
+                            <Text style ={styles.title}>{box.name}</Text>          
+                    </TouchableOpacity>  
+                    }
+            return (
             <TouchableOpacity style={styles.area} onPress = {this.handlePress}>
-            <View style ={styles.info}>
-            <Image style ={styles.image} 
-            source={require('../catimages/59085af44bc6b0d3074933b6.jpeg')}
-             /> 
-                <Text style ={styles.title}>{box.name}</Text>          
-            </View>
-        </TouchableOpacity>        
-        );
+         
+        </TouchableOpacity>   */     
     }
 }
 export default CategoryItem;
@@ -48,6 +101,7 @@ const styles = StyleSheet.create({
         height:150,
     },
     info: {
+        marginTop:5,
         padding:10,
         backgroundColor:'#fff',
         borderColor:'#bbb',
